6529a19f64efbe2586e91a206325087fb29ddb0b
Gaurav Ashtikar <gashtikar@spotnana.com>
2024-03-13 02:27:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -161,7 +161,7 @@ resource "aws_msk_cluster" "this" {
   # required for appautoscaling
   lifecycle {
     ignore_changes = [
-      broker_node_group_info[0].storage_info[0].ebs_storage_info[0].volume_size,
+      broker_node_group_info[0].storage_info[0].ebs_storage_info[0].volume_size, client_authentication[0].tls
     ]
   }
 
